3915c6cf02bc03860c32d99eb800b91db4c777ce
Hun-Hong <adeliae.p1841@gmail.com>
2025-11-19 16:55:27 +0900

    [Solved] '찾아오시는 길' 문제 풀이

---
+++ 202511/BOJ34217.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    A, B = map(int, input().split())
+    C, D = map(int, input().split())
+
+    if (A + C) > (B + D):
+        print("Yongdap")
+    elif (A + C) < (B + D):
+        print("Hanyang Univ.")
+    else:
+        print("Either")
